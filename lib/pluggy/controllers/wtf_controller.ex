a7acc2c9ddeebd73e775d6aab8ee88080190a4d2
itggot-erik-landmark <erik.landmark@itggot.se>
2018-09-11 16:01:26 +0200

    Fixed head

--- lib/pluggy/controllers/wtf_controller.ex
+++ lib/pluggy/controllers/wtf_controller.ex
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-defmodule Pluggy.WTFController do
-  
-  require IEx
-
-  alias Pluggy.Wtf
-  alias Pluggy.User
-  import Pluggy.Template, only: [render: 2]
-  import Plug.Conn, only: [send_resp: 3]
-
-
-  def index(conn) do
-    send_resp(conn, 200, render("whatsTheirFace/index", []))
-  end
-
-  def login(conn) do
-    send_resp(conn, 200, render("whatsTheirFace/login", []))
-  end
-
-  def home(conn) do
-
-    #get user if logged in
-    session_user = conn.private.plug_session["user_id"]
-    current_user = case session_user do
-      nil -> nil
-      _   -> User.get(session_user)
-    end
-
-    send_resp(conn, 200, render("whatsTheirFace/home", wtf: Students.all(), user: current_user))
-  end
-
-  defp redirect(conn, url) do
-    Plug.Conn.put_resp_header(conn, "location", url) |> send_resp(303, "")
-  end
-
-end
-=======
 defmodule Pluggy.WTFController do
   
   require IEx
@@ -91,4 +54,3 @@ defmodule Pluggy.WTFController do
   end
 
 end
->>>>>>> 65df44b24343edf43230d060eb965440640d9590
--- lib/pluggy/models/classes.ex
+++ lib/pluggy/models/classes.ex
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 defmodule Pluggy.Classes do
 
   defstruct(id: nil, name: "")
@@ -35,42 +34,4 @@ defmodule Pluggy.Classes do
   def to_struct_list(rows) do
     for [id, name] <- rows, do: %Pluggy.Classes{id: id, name: name}
   end
-=======
-defmodule Pluggy.Classes do
-
-  defstruct(id: nil, name: "")
-
-  def all do
-    Postgrex.query!(DB, "SELECT * FROM classes", [], [pool: DBConnection.Poolboy]).rows
-    |> to_struct_list
-  end
-
-  def get_by_id(id) do
-    Postgrex.query!(DB, "SELECT * FROM classes WHERE id = $1 LIMIT 1", [String.to_integer(id)], [pool: DBConnection.Poolboy]).rows
-    |> to_struct
-  end
-
-  def update(id, params) do
-    name = params["name"]
-    id = String.to_integer(id)
-    Postgrex.query!(DB, "UPDATE classes SET name = $1, WHERE id = $2", [name, id], [pool: DBConnection.Poolboy])
-  end
-
-  def create(params) do
-    name = params["name"]
-    Postgrex.query!(DB, "INSERT INTO classes (name) VALUES ($1)", [name], [pool: DBConnection.Poolboy])
-  end
-
-  def delete_by_id(id) do
-    Postgrex.query!(DB, "DELETE FROM classes WHERE id = $1", [String.to_integer(id)], [pool: DBConnection.Poolboy])
-  end
-
-  def to_struct([[id, name, tastiness]]) do
-    %Pluggy.Students{id: id, name: name}
-  end
-
-  def to_struct_list(rows) do
-    for [id, name] <- rows, do: %Pluggy.Classes{id: id, name: name}
-  end
->>>>>>> 65df44b24343edf43230d060eb965440640d9590
 end
