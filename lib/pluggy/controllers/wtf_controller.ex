d89b98fa53adae855022a1674baebf70d461c988
itggot-elias-nordh <elias.nordh@itggot.se>
2018-09-12 14:08:09 +0200

    fixat så att man kan välja klass och svårihet

--- lib/pluggy/controllers/wtf_controller.ex
+++ lib/pluggy/controllers/wtf_controller.ex
@@ -2,7 +2,7 @@ defmodule Pluggy.WTFController do
 
   require IEx
 
-  alias Pluggy.Wtf
+  alias Pluggy.Students
   alias Pluggy.User
   import Pluggy.Template, only: [render: 2]
   import Plug.Conn, only: [send_resp: 3]
@@ -15,6 +15,66 @@ defmodule Pluggy.WTFController do
   def login(conn) do
     send_resp(conn, 200, render("whatsTheirFace/login", []))
   end
+
+  def difficulty(conn) do
+    send_resp(conn, 200, render("whatsTheirFace/difficulty", []))
+  end
+
+  def quiz1(conn) do
+    conn = Plug.Conn.put_session(conn, :times, conn.private.plug_session["times"] + 1)
+    if conn.private.plug_session["start"] == nil or conn.private.plug_session["start"] == false do
+        redirect(conn, "/wtf")
+    end
+    className = conn.private.plug_session["className"]
+    difficulty = conn.private.plug_session["difficulty"]
+    send_resp(conn, 200, render("whatsTheirFace/quiz1", className: className))
+  end
+
+  get('/game1') do
+    session[:times] += 1
+    if session[:start] == nil or session[:start] == false
+        redirect('/')
+    end
+    className = session[:className]
+    difficulty = session[:difficulty].to_i
+    names = getList()[0][className].shuffle
+    if difficulty == 2
+    elsif difficulty == 1
+        size = (names.length/2).to_i
+        names = names[0...size]
+    else
+        size = (names.length/4).to_i
+        names = names[0...size]
+    end
+    if names.length < 4
+        session[:error] = "Too few students"
+    session[:back] = "/"
+    halt 404
+      end
+      if session[:times] > 1
+          session[:error] = "You are not allowed to view this page"
+          if session[:times] > names.length
+              session[:back] = "/score"
+          else
+              session[:cheat] += 1
+              session[:times] = session[:times] - 2
+              session[:back] = "/game2/#{session[:times] - 1}"
+          end
+          halt 404
+      end
+      session[:names] = names.shuffle
+      slim(:game1, locals:{className: className, names: names})
+  end
+
+  def quiz_start(conn, params) do
+    conn = Plug.Conn.put_session(conn, :start, true)
+    conn = Plug.Conn.put_session(conn, :times, 0)
+    conn = Plug.Conn.put_session(conn, :cheat, 0)
+    conn = Plug.Conn.put_session(conn, :className, params["className"])
+    conn = Plug.Conn.put_session(conn, :difficulty, params["difficulty"])
+    conn = Plug.Conn.put_session(conn, :score, true)
+    redirect(conn, "/quiz1")
+  end  
 
   def home(conn) do
 
